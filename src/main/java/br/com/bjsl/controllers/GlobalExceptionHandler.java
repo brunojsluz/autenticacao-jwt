66d4cf6a1779a2cc5bdc7994c84b236e6798df58
Bruno Luz <brunojsluz@gmail.com>
2019-12-30 16:17:26 -0300

    Implementa interceptor de erros

--- src/main/java/br/com/bjsl/controllers/GlobalExceptionHandler.java
+++ src/main/java/br/com/bjsl/controllers/GlobalExceptionHandler.java
@@ -4,4 +4,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+  
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map handle(MethodArgumentNotValidException exception) {
+        return error(exception.getBindingResult().getFieldErrors()
+                             .stream()
+                             .map(FieldError::getDefaultMessage)
+                             .collect(Collectors.toList()));
+    }
+  
+    private Map error(Object message) {
+      return Collections.singletonMap("error", message);
+    }
 }
